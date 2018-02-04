6d979e2eb0a63492b6fe0586949352fe2e037001
Mathieu PINSON <mpinson@e1r8p21.42.fr>
2018-02-04 23:01:29 +0100

    ok

--- 01_dockerfiles/ex01/script.sh
+++ 01_dockerfiles/ex01/script.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-# complier le serveur avec docker build -t <nom/nom> .
 # pour lancer le serveur
-# docker run  --name ts3 -p 9987:9987/udp -p 10011:10011 -ti -p  30033:30033 <nom/nom>
+# docker run -p 9987:9987/udp -p 10011:10011 -ti -p  30033:30033
 
 sh /teamspeak3-server_linux-amd64/ts3server_minimal_runscript.sh
